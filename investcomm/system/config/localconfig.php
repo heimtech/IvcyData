8eb4ac58e7ec94c964f6b751ea12e0ae7b24ba94
heimtech <dominik@heim-tech.de>
2017-01-19 19:34:10 +0100

    h

---
+++ investcomm/system/config/localconfig.php
@@ -0,0 +1,21 @@
+<?php
+
+### INSTALL SCRIPT START ###
+$GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
+$GLOBALS['TL_CONFIG']['installPassword'] = '$2y$10$BbQ9We84sXnZNsIUaVl2secyZ4Ed6e569XerlJqZZEQsNcug3E2oa';
+$GLOBALS['TL_CONFIG']['encryptionKey'] = '7d17255b0cf6bb2bbe389232615d3b40';
+$GLOBALS['TL_CONFIG']['installCount'] = 0;
+$GLOBALS['TL_CONFIG']['dbDriver'] = 'MySQL';
+$GLOBALS['TL_CONFIG']['dbHost'] = 'mysql5.heim-tech.de';
+$GLOBALS['TL_CONFIG']['dbUser'] = 'db459210_14';
+$GLOBALS['TL_CONFIG']['dbPass'] = 'fqhpu-V5pRtL';
+$GLOBALS['TL_CONFIG']['dbDatabase'] = 'db459210_14';
+$GLOBALS['TL_CONFIG']['dbPconnect'] = false;
+$GLOBALS['TL_CONFIG']['dbCharset'] = 'UTF8';
+$GLOBALS['TL_CONFIG']['dbPort'] = 3306;
+$GLOBALS['TL_CONFIG']['dbSocket'] = '';
+$GLOBALS['TL_CONFIG']['maintenanceMode'] = false;
+$GLOBALS['TL_CONFIG']['latestVersion'] = '3.5.22';
+$GLOBALS['TL_CONFIG']['useSMTP'] = false;
+$GLOBALS['TL_CONFIG']['displayErrors'] = true;
+### INSTALL SCRIPT STOP ###
